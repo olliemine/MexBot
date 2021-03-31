357d9c9103db4e26281853f0f5d781e1fb700852
YTOliver <73561517+YTOliver@users.noreply.github.com>
2021-03-31 13:18:48 -0600

    Spaghetti update

--- index.js
+++ index.js
@@ -86,13 +86,32 @@ for(const file of commandFiles) {
 
 async function UpdateUsers() {
 	await mongo().then(async () => {
-		UserList = await UserSchema.find({ active: true })
+		const UserList = await UserSchema.find({ active: true })
 		const server = await client.guilds.fetch("822514160154706010")
+		const ranks = [server.roles.cache.get("823061333020246037"), server.roles.cache.get("823061825154580491"), server.roles.cache.get("824786196077084693"), server.roles.cache.get("824786280616689715")]
 		UserList.forEach(async (user) => {
 			await fetch(`https://new.scoresaber.com/api/player/${user.beatsaber}/full`).then(res => res.json()).then(async (body) => {
 				if(body.error) return console.log("Coudnt fetch user " + user.discord)
 				if(user.lastrank == body.playerInfo.countryRank) return
 				const discorduser = await server.members.fetch(user.discord)
+				
+				if(body.playerInfo.countryRank <= 10) {//Top 10?
+					if(!discorduser.roles.cache.find(r => r.id === ranks[0].id)) discorduser.roles.add(ranks[0])//Checkar si tiene role y si no dar role
+					if(body.playerInfo.countryRank <= 3) {//Es top 3?
+						if(!discorduser.roles.cache.find(r => r.id === ranks[body.playerInfo.countryRank])) {//Tiene el role?
+							discorduser.roles.add(ranks[body.playerInfo.countryRank])
+							for (let index = 1; index <= 3; index++) {
+								if(index == body.playerInfo.countryRank) continue
+								if(body.playerInfo.countryRank != index && discorduser.roles.cache.find(r => r.id === ranks[index])) discorduser.roles.remove(ranks[index])
+							}
+						}
+					} else if(discorduser.roles.cache.find(r => r.id === ranks[1]) || discorduser.roles.cache.find(r => r.id === ranks[2]) || discorduser.roles.cache.find(r => r.id === ranks[3])) { //Quitar roles y return
+						for (let index = 1; index <= 3; index++) {
+							discorduser.roles.remove(ranks[index])
+						}
+					}
+				} else if(discorduser.roles.cache.find(r => r.id === ranks[0].id)) discorduser.roles.remove(ranks[0]) //Quitar role y return
+				
 				discorduser.setNickname(`#${body.playerInfo.countryRank} | ${body.playerInfo.playerName}`)
 				await UserSchema.findOneAndUpdate({
 					discord: user.discord
@@ -105,7 +124,7 @@ async function UpdateUsers() {
 }
 setInterval(() => {
 	UpdateUsers()
-}, (1000*60)*60)//1h
+}, (1000*60)*30)//30m
 
 async function Verificacion(member, msg) {
 	if(+msg.content) {
