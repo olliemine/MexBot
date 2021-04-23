b58abda79d246897a8900af9deab4f3514931d29
YTOliver <73561517+YTOliver@users.noreply.github.com>
2021-04-23 17:24:53 -0500

    tiny forceupdate thing

--- commands/forceupdate.js
+++ commands/forceupdate.js
@@ -6,6 +6,6 @@ module.exports = {
 	async execute(message, DiscordClient) {
 		if(!message.member.roles.cache.find(r => r.id === "822553320551874650")) return
 		await UpdateUsers(DiscordClient)
-		message.channel.send("Succesfully updated users!")
+		message.channel.send(`Succesfully updated users! took ${Date.now() - message.createdTimestamp}ms to execute!`)
 	}
 }
