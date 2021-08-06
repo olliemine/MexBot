eaa7354e6ef0fd3dada3c320d07e7da43640302c
YTOliver <olliemine@outlook.com>
2021-08-06 15:13:03 +0200

    Made CheckRoles easier to read

--- functions/CheckRoles.js
+++ functions/CheckRoles.js
@@ -1,23 +1,38 @@
-//Note, this fucking sucks and is really hard to fucking read
-//I will also probably never change this
+//This is now easier to read, I am much happy :D
 
 module.exports = (rank, discorduser, ranks) => {
-    if(rank <= 10) {//Top 10?
-        if(!discorduser.roles.cache.find(r => r.id === ranks[0].id)) discorduser.roles.add(ranks[0])//Checkar si tiene role y si no dar role
-        if(rank <= 3) {//Es top 3?
-            if(!discorduser.roles.cache.find(r => r.id === ranks[rank].id)) {//Tiene el role?
-                discorduser.roles.add(ranks[rank])
-                for (let index = 1; index <= 3; index++) {
-                    if(index == rank) continue
-                    if(discorduser.roles.cache.find(r => r.id === ranks[index].id)) discorduser.roles.remove(ranks[index])
-                }
-            }
-        } else if(discorduser.roles.cache.find(r => r.id === ranks[1].id) || discorduser.roles.cache.find(r => r.id === ranks[2].id) || discorduser.roles.cache.find(r => r.id === ranks[3].id)) { //Quitar roles y return
-            for (let index = 1; index <= 3; index++) {
-                discorduser.roles.remove(ranks[index])
-            }
+    function Check(rank) {
+        return discorduser.roles.cache.find(r => r.id === rank.id)
+    }
+    function Remove(rank) {
+        return discorduser.roles.remove(rank)
+    }
+    function Add(rank) {
+        return discorduser.roles.add(rank)
+    }
+    function CheckAndRemove() {
+        for(let index = 1; index <= 3; index++) {
+            if(index == rank) continue
+            if(Check(ranks[index])) Remove(ranks[index])
         }
-    } else if(discorduser.roles.cache.find(r => r.id === ranks[0].id)) discorduser.roles.remove(ranks[0]) //Quitar role y return
+        return
+    }
+    
+    if(rank >= 10) {
+        if(Check(ranks[0])) Remove(ranks[0])
+        return
+    }
+    // Is 10
+    if(!Check(ranks[0])) Add(ranks[0])
+    if(rank >= 3) {
+        if(Check(ranks[1]) || Check(ranks[2]) || Check(ranks[3])) CheckAndRemove()
+        return
+    }
+    //Is 3
+    if(!Check(ranks[rank])) {
+        Add(ranks[rank])
+        CheckAndRemove()
+    }
 }
 //const server = await DiscordClient.guilds.fetch("822514160154706010")
 //const ranks = [server.roles.cache.get("823061333020246037"), server.roles.cache.get("823061825154580491"), server.roles.cache.get("824786196077084693"), server.roles.cache.get("824786280616689715")]
