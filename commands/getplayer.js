cd6e64fefb9de171ab60bc642b4710a4cb23280e
YTOliver <olliemine@outlook.com>
2021-08-23 15:35:14 +0200

    Cleaned getplayer code

--- commands/getplayer.js
+++ commands/getplayer.js
@@ -14,7 +14,6 @@ module.exports = {
 	dm: true,
 	cooldown: 4,
 	async execute(message, DiscordClient, args) {
-		let extra_playerinfo
 		if (!Array.isArray(args) || !args.length) {
 			try {
 				user = await UserSchema.findOne({ discord: message.author.id })
@@ -86,94 +85,63 @@ module.exports = {
 			if(number > 0) return "+"
 			return ""
 		}
-		function lateFooter() {
-			if(Date.now() - message.createdTimestamp >= 1000*5) return "El lag fue un problema de scoresaber, no de el bot"
-			return ""
+		function ErrorEmbed(description) {
+			const embed = new MessageEmbed()
+			.setTitle("Error!")
+			.setDescription(description)
+			.setColor("#F83939")
+			return message.channel.send({ embeds: [embed]})
+		}
+		function BuildEmbed(data) {
+			const history = data.playerInfo.history.split(",")
+			const embed = new MessageEmbed()
+			.setColor("#4C9CF6")
+			.setTitle(data.playerInfo.playerName + ` :flag_${data.playerInfo.country.toLowerCase()}:`)
+			.setURL(`https://scoresaber.com/u/${data.playerInfo.playerId}`)
+			.setThumbnail(`https://new.scoresaber.com${data.playerInfo.avatar}`)
+			.addField("PP", `${numberWithCommas(data.playerInfo.pp.toFixed(1))}pp
+Week difference: ${Addplus(history[history.length - 7] - data.playerInfo.rank)}${history[history.length - 7] - data.playerInfo.rank}`)
+			.addField("RANK", `Rank: #${data.playerInfo.rank}
+Country rank: #${data.playerInfo.countryRank}`)
+			.addField("RANKED", `Average Accuracy: ${data.scoreStats.averageRankedAccuracy.toFixed(2)}%
+Ranked playcount: ${data.scoreStats.rankedPlayCount}`)
+			.addField("PP Calculation", "Loading...")
+			message.channel.send({ embeds: [embed]}).then(msg => {
+				return getOneConvertedPP(data.playerInfo.playerId, embed, msg)
+			})
 		}
 		function GetPlayerDataID(Id) {
 			const IDURL = new URL(`https://new.scoresaber.com/api/player/${Id}/full`)
 			fetch(IDURL)
-			.then(res => res.json())
-			.then(async body => {
-				if(body.error) { //Error handler
-					const embed = new MessageEmbed()
-					.setTitle("Error!")
-					.setDescription(body.error.message)
-					.setColor("#F83939")
-					.setFooter(lateFooter())
-					return message.channel.send({ embeds: [embed]})
-				}
-				const history = body.playerInfo.history.split(",")
-				const embed = new MessageEmbed()
-				.setColor("#4C9CF6")
-				.setTitle(body.playerInfo.playerName + ` :flag_${body.playerInfo.country.toLowerCase()}:`)
-				.setURL(`https://scoresaber.com/u/${body.playerInfo.playerId}`)
-				.setThumbnail(`https://new.scoresaber.com${body.playerInfo.avatar}`)
-				.setFooter(lateFooter())
-				.addField("PP", `${numberWithCommas(body.playerInfo.pp.toFixed(1))}pp
-Week difference: ${Addplus(history[history.length - 7] - body.playerInfo.rank)}${history[history.length - 7] - body.playerInfo.rank}`)
-				.addField("RANK", `Rank: #${body.playerInfo.rank}
-Country rank: #${body.playerInfo.countryRank}`)
-				.addField("RANKED", `Average Accuracy: ${body.scoreStats.averageRankedAccuracy.toFixed(2)}%
-Ranked playcount: ${body.scoreStats.rankedPlayCount}`)
-				.addField("PP Calculation", "Loading...")
-				.setFooter("Disclamer: El PP Calculation puede ser que este incorecto")
-				message.channel.send({ embeds: [embed]}).then(msg => {
-					return getOneConvertedPP(Id, embed, msg)
-				})
-			}).catch((err) => {
-				message.channel.send({ content: "Parece que hay un error con scoresaber, porfavor intenta despues"})
-				errorhandle(DiscordClient, err)
+			.then(async res => {
+				if(res.status == 429) return ErrorEmbed("Api Overloaded! Please try again in some seconds")
+				if(res.status != 200) return ErrorEmbed(`Unknown Error ${res.status} ${res.statusText}`)
+				const body = await res.json()
+				BuildEmbed(body)
 			})
 		}
 		function GetPlayerDataName(name) {
 			if(name.length <= 3 || name.length > 32) return message.channel.send({content: "Invalid Name length"})
 			const NAMEURL = new URL(`https://new.scoresaber.com/api/players/by-name/${name}`)
 			fetch(NAMEURL)
-			.then(res => res.json())
-			.then(async info => {
-			if(info.error) { //Error handler
-				if(+name) return GetPlayerDataID(name)
-				const embed = new MessageEmbed()
-				.setTitle("Error!")
-				.setDescription(info.error.message)
-				.setColor("#F83939")
-				.setFooter(lateFooter())
-				return message.channel.send({embeds: [embed]})
-			}
-			const player = info.players[0]
-			await fetch(`https://new.scoresaber.com/api/player/${player.playerId}/full`).then(res => res.json()).then(body => extra_playerinfo = body)
-			const embed = new MessageEmbed()
-			.setColor("#4C9CF6")
-			.setTitle(player.playerName + ` :flag_${player.country.toLowerCase()}:`)
-			.setURL(`https://scoresaber.com/u/${player.playerId}`)
-			.setThumbnail(`https://new.scoresaber.com${player.avatar}`)
-			.addField("PP", `${numberWithCommas(player.pp.toFixed(1))}pp
-Week difference: ${Addplus(player.difference)}${player.difference}`, false)
-			.addField("RANK", `Rank: #${player.rank}
-Country rank: #${extra_playerinfo.playerInfo.countryRank}`, false)
-			.addField("RANKED", `Average Accuracy: ${extra_playerinfo.scoreStats.averageRankedAccuracy.toFixed(2)}%
-Ranked playcount: ${extra_playerinfo.scoreStats.rankedPlayCount}`)
-			.addField("PP Calculation", "Loading...")
-			.setFooter("Disclamer: El PP Calculation puede ser que este incorecto")
-			if(Date.now() - message.createdTimestamp >= 1000*5) {
-				const user = {
-					name: args.join(" ").toLowerCase(),
-					id: player.playerId
+			.then(async res => {
+				if(res.status == 429) return ErrorEmbed("Api Overloaded! Please try again in some seconds")
+				if(res.status == 404 && +name) return GetPlayerDataID(name)
+				if(res.status != 200) return ErrorEmbed(`Unknown Error ${res.status} ${res.statusText}`)
+				const body = await res.json()
+				if(Date.now() - message.createdTimestamp >= 1000*5) {
+					const user = {
+						name: args.join(" ").toLowerCase(),
+						id: player.playerId
+					}
+					try {
+						await new UserCacheSchema(user).save()
+					} catch(err) {
+						errorhandle(DiscordClient, err)
+					}
 				}
-				try {
-					await new UserCacheSchema(user).save()
-				} catch(err) {
-					errorhandle(DiscordClient, err)
-				}
-			}
-			message.channel.send({embeds: [embed]}).then(msg => {
-				return getOneConvertedPP(player.playerId, embed, msg)
+				GetPlayerDataID(body.players[0].playerId)
 			})
-		}).catch((err) => {
-			message.channel.send({content: "Parece que hay un error con scoresaber, porfavor intenta despues"})
-			errorhandle(DiscordClient, err)
-		})
 		}
 	}
 }
