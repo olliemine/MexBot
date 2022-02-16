dbc2b4a536449e2a1f8aca8abd87ede42294c6b6
YTOliver <73561517+YTOliver@users.noreply.github.com>
2022-02-15 18:13:31 -0600

    1.6.3.0 oop sorry

--- functions/TopFeed/Score.js
+++ functions/TopFeed/Score.js
@@ -1,18 +1,18 @@
 module.exports = (m) => {
 	return {
-		map = m.leaderboard.id,
-		songName = m.leaderboard.songName,
-		songAuthorName = m.leaderboard.songAuthorName,
-		mapAuthor = m.leaderboard.levelAuthorName,
-		score = m.score.baseScore,
-		hash = m.leaderboard.songHash.toUpperCase(),
-		diff = m.leaderboard.difficulty.difficultyRaw,
-		diffSort = m.leaderboard.difficulty.difficulty,
-		date = m.score.timeSet,
-		mods = m.score.modifiers.split(","), 
-		pp = m.score.pp.toFixed(1),
-		ranked = m.leaderboard.ranked,
-		stars = m.leaderboard.stars,
-		maxscore = m.leaderboard.maxScore
+		map: m.leaderboard.id,
+		songName: m.leaderboard.songName,
+		songAuthorName: m.leaderboard.songAuthorName,
+		mapAuthor: m.leaderboard.levelAuthorName,
+		score: m.score.baseScore,
+		hash: m.leaderboard.songHash.toUpperCase(),
+		diff: m.leaderboard.difficulty.difficultyRaw,
+		diffSort: m.leaderboard.difficulty.difficulty,
+		date: m.score.timeSet,
+		mods: m.score.modifiers.split(","), 
+		pp: m.score.pp.toFixed(1),
+		ranked: m.leaderboard.ranked,
+		stars: m.leaderboard.stars,
+		maxscore: m.leaderboard.maxScore
 	}
 }
